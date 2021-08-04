6d13235c138dedfa443ede5ef9275ff0de4204ea
shanfeng <1218154980@qq.com>
2021-08-04 09:32:58 +0800

    项目第一次提交

---
+++ src/components/common/tabbar/TaBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-item" @click="itemClick">
+    <div v-if="!isactive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem',
+  props:{
+    path:String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  data() {
+    return {
+      // isactive:true
+
+    }
+  },
+  computed:{
+    isactive(){
+      return this.$route.path.indexOf(this.path)!== -1
+    },
+    activeStyle(){
+      return this.isactive?{color:this.activeColor}:{}
+    }
+  },
+  methods: {
+    itemClick(){
+      this.$router.replace(this.path)
+    }
+  },
+}
+</script>
+
+<style>
+.tab-item{
+    flex: 1;
+    height: 49px;
+    text-align: center;
+    font-size: 14px;
+  }
+  .tab-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from '../../components/common/navbar/NavBar.vue'
+import {getHomeMultidata} from '../../network/home'
+  export default {
+    name: "Home",
+    components:{
+      NavBar
+    },
+    data() {
+      return {
+        result:null
+      }
+    },
+    // 组件创建完后发送网络请求
+    created(){
+      // 1.请求多个数据
+      getHomeMultidata().then(res=>{
+        // console.log(res);
+        this.result=res;
+      })
+
+    }
+  
+
+  }
+</script>
+
+<style >
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
